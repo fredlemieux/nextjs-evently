598d788ecf73ec4075a134c854529c859020d499
Frederique Lemieux <fredlemi@gmail.com>
2024-10-16 23:09:31 +0200

    Auto focus on input on load

--- src/components/shared/Dropdown.tsx
+++ src/components/shared/Dropdown.tsx
@@ -6,7 +6,7 @@ import {
   SelectValue,
 } from '@/components/ui/select';
 import { ICategory } from '@/lib/database/models/category.model';
-import { startTransition, useEffect, useState } from 'react';
+import { startTransition, useEffect, useRef, useState } from 'react';
 import {
   AlertDialog,
   AlertDialogAction,
@@ -32,16 +32,20 @@ type DropdownProps = {
 const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
   const [categories, setCategories] = useState<ICategory[]>([]);
   const [newCategory, setNewCategory] = useState('');
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
 
-  const handleAddCategory = () => {
-    createCategory({
-      categoryName: newCategory.trim(),
-    }).then((category) => {
-      if (category) {
-        setCategories((prevState) => [...prevState, category]);
-      }
+  const handleAddCategory = () =>
+    startTransition(() => {
+      createCategory({
+        categoryName: newCategory.trim(),
+      }).then((category) => {
+        if (category) {
+          setCategories((prevState) => [...prevState, category]);
+        }
+        setIsDialogOpen(false);
+      });
     });
-  };
 
   useEffect(() => {
     const getCategories = async () => {
@@ -54,6 +58,14 @@ const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
 
     getCategories();
   }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (isDialogOpen && inputRef.current) {
+        inputRef.current.focus();
+      }
+    }, 100);
+  }, [isDialogOpen]);
 
   return (
     <Select onValueChange={onChangeHandler} defaultValue={value}>
@@ -72,27 +84,30 @@ const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
             </SelectItem>
           ))}
 
-        <AlertDialog>
+        <AlertDialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
           <AlertDialogTrigger className='p-medium-14 flex w-full rounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500'>
             Add new category
           </AlertDialogTrigger>
+
           <AlertDialogContent className='bg-white'>
             <AlertDialogHeader>
               <AlertDialogTitle>New Category</AlertDialogTitle>
               <AlertDialogDescription>
                 <Input
+                  ref={inputRef}
                   type='text'
                   placeholder='Category name'
                   className='input-field mt-3'
                   onChange={(e) => setNewCategory(e.target.value)}
+                  onKeyDown={(e) =>
+                    e.key === 'Enter' ? handleAddCategory() : undefined
+                  }
                 />
               </AlertDialogDescription>
             </AlertDialogHeader>
             <AlertDialogFooter>
               <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction
-                onClick={() => startTransition(handleAddCategory)}
-              >
+              <AlertDialogAction onClick={() => handleAddCategory()}>
                 Add
               </AlertDialogAction>
             </AlertDialogFooter>
