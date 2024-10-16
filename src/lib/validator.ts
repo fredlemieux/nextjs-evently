c7ae05555e8988987b951f5d29b449aeb43d4a7c
Frederique Lemieux <fredlemi@gmail.com>
2024-10-16 21:47:16 +0200

    Add zod validation for event start and end dateTime

--- src/lib/validator.ts
+++ src/lib/validator.ts
@@ -11,10 +11,13 @@ export const eventFormSchema = z.object({
     .min(3, 'Location must be at least 3 characters')
     .max(400, 'Location must be less than 400 characters'),
   imageUrl: z.string(),
-  startDateTime: z.date(),
+  startDateTime: z.date().refine(startDateTime => startDateTime > new Date(), {message: "Start of event cannot be in the past"}),
   endDateTime: z.date(),
   categoryId: z.string().min(1, "Please select a category"),
   price: z.string(),
   isFree: z.boolean(),
   url: z.string().url().or(z.string().max(0)),
+}).refine(data => data.endDateTime > data.startDateTime, {
+  message: "End cannot before the start! It's impossible.",
+  path: ['endDateTime']
 });
