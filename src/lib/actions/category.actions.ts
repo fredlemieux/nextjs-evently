b3c9bc0466408b4ac66855139c737206b98cb012
Frederique Lemieux <fredlemi@gmail.com>
2024-10-22 15:04:18 +0200

    Use documentToJson for the getAllCategories

--- src/lib/actions/category.actions.ts
+++ src/lib/actions/category.actions.ts
@@ -4,7 +4,8 @@ import { CreateCategoryParams } from '@/types/parameters.types';
 import { handleError } from '../utils';
 import { connectToDatabase } from '../database';
 import { Category, ICategory } from '@/lib/database/models';
-// import { documentToJson } from '@/lib/actions/utils.actions';
+import { HydratedDocument } from 'mongoose';
+import { documentToJson } from '@/lib/actions/utils.actions';
 
 export const createCategory = async ({
   categoryName,
@@ -24,11 +25,9 @@ export const getAllCategories = async (): Promise<ICategory[] | undefined> => {
   try {
     await connectToDatabase();
 
-    const categories = await Category.find();
+    const categories: HydratedDocument<ICategory>[] = await Category.find();
 
-    // TODO! Why doesn't this work?
-    // return documentToJson<ICategory[]>(categories);
-    return JSON.parse(JSON.stringify(categories));
+    return documentToJson(categories);
   } catch (error) {
     handleError(error);
   }
