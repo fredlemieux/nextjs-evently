e0412e665556edaa4981644d72a30d8d0307de4c
Frederique Lemieux <fredlemi@gmail.com>
2024-10-22 23:52:07 +0200

    Add all env types

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -2,6 +2,11 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
     CLERK_SECRET_KEY: string;
+    WEBHOOK_SECRET: string;
     MONGODB_URI: string;
+    UPLOADTHING_TOKEN: string;
+    UPLOADTHING_SECRET: string;
+    UPLOADTHING_APP_ID: string;
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: string;
   }
 }
