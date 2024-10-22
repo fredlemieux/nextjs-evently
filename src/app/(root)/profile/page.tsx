b86258527b078700780e7bbb174062178905843f
Frederique Lemieux <fredlemi@gmail.com>
2024-10-22 23:44:35 +0200

    Remove all stripe related pages, components, models and actions

--- src/app/(root)/profile/page.tsx
+++ src/app/(root)/profile/page.tsx
@@ -1,8 +1,6 @@
 import Collection from '@/components/shared/Collection';
 import { Button } from '@/components/ui/button';
 import { getEventsByUser } from '@/lib/actions/event.actions';
-import { getOrdersByUser } from '@/lib/actions/order.actions';
-import { IOrder } from '@/lib/database/models/order.model';
 import { SearchParamProps } from '@/types/parameters.types';
 import { auth } from '@clerk/nextjs';
 import Link from 'next/link';
@@ -12,12 +10,8 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
 
-  const ordersPage = Number(searchParams?.ordersPage) || 1;
   const eventsPage = Number(searchParams?.eventsPage) || 1;
 
-  const orders = await getOrdersByUser({ userId, page: ordersPage });
-
-  const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
   const organizedEvents = await getEventsByUser({ userId, page: eventsPage });
 
   return (
@@ -29,19 +23,6 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
             <Link href='/#events'>Explore More Events</Link>
           </Button>
         </div>
-      </section>
-
-      <section className='wrapper my-8'>
-        <Collection
-          data={orderedEvents}
-          emptyTitle='No event tickets purchased yet'
-          emptyStateSubtext='No worries - plenty of exciting events to explore!'
-          collectionType='My_Tickets'
-          limit={3}
-          page={ordersPage}
-          urlParamName='ordersPage'
-          totalPages={orders?.totalPages}
-        />
       </section>
 
       <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
--- src/lib/database/models/index.ts
+++ src/lib/database/models/index.ts
@@ -1,5 +1,4 @@
 export * from './category.model';
 export * from './event.model';
-export * from './order.model';
 export * from './user.model';
 export * from './location.model';
