ea9f15e20d7625a87d08f3431e448a29605aab9b
Frederique Lemieux <fredlemi@gmail.com>
2024-10-29 14:10:52 +0100

    Use new clerkMiddleWare and createRouteMatchers, add routes to protect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,30 @@
-import { authMiddleware } from '@clerk/nextjs';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+//
+// export default clerkMiddleware({
+//   // debug: true, // Use to debug Auth issues
+//   publicRoutes: ['/', 'events/:id', '/api/webhook/clerk', '/api/uploadthing'],
+//   ignoredRoutes: ['/api/webhook/clerk'],
+// });
+//
+// export const config = {
+//   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
 
-export default authMiddleware({
-  // debug: true, // Use to debug Auth issues
-  publicRoutes: ['/', 'events/:id', '/api/webhook/clerk', '/api/uploadthing'],
-  ignoredRoutes: ['/api/webhook/clerk'],
+const isProtectedRoute = createRouteMatcher([
+  '/events/create',
+  '/events/(.*)/update',
+  '/profile',
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect();
 });
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
