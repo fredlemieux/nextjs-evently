d798f7dd3357ae7a993e0d118e1a348d5fd769e1
Frederique Lemieux <fredlemi@gmail.com>
2024-10-30 22:40:08 +0100

    Update location.seed.ts file to seedMultipleLocations, and also update function to seedLocation, which has better simantics

--- test/seeds/location.seed.ts
+++ test/seeds/location.seed.ts
@@ -1,7 +1,13 @@
 import { ILocation, LocationModel } from '@/lib/database/models';
 import { genLocationMock } from '@test/data/location.data';
 
-export async function locationSeed(): Promise<ILocation> {
+export async function seedLocation(): Promise<ILocation> {
   const locationMock = genLocationMock();
   return await LocationModel.create(locationMock);
+}
+
+export async function seedMultipleLocations(
+  numberOfSeeds: number = 5
+): Promise<ILocation[]> {
+  return Promise.all([...Array(numberOfSeeds)].map(() => seedLocation()));
 }
