df7f60d9b00661e5e887aae03e92ceb7f8e82e5e
Frederique Lemieux <fredlemi@gmail.com>
2024-10-30 22:39:09 +0100

    Update the global setup so that we use one MongoDb instance per test suite

--- test/globalSetup.ts
+++ test/globalSetup.ts
@@ -1,24 +1,24 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import * as mongoose from 'mongoose';
 import { config } from './utils/config';
+import { uuidv4 } from 'mongodb-memory-server-core/lib/util/utils';
 
 export default async function globalSetup() {
   if (config.Memory) {
-    // Config to decide if an mongodb-memory-server instance should be used
-    // it's needed in global space, because we don't want to create a new instance every test-suite
+    // Global MongoMemoryServer instance
     const instance = await MongoMemoryServer.create();
     const uri = instance.getUri();
     (global as any).__MONGOINSTANCE = instance;
-    process.env.MONGODB_URI = uri.slice(0, uri.lastIndexOf('/'));
+
+    // Generate a unique database name for each test suite
+    const uniqueDbName = uuidv4();
+    process.env.MONGODB_URI = `${uri.slice(0, uri.lastIndexOf('/'))}/${uniqueDbName}`;
   } else {
-    process.env.MONGODB_URI = `mongodb://${config.IP}:${config.Port}`;
+    process.env.MONGODB_URI = `mongodb://${config.IP}:${config.Port}/${config.Database}`;
   }
 
-  // The following is to make sure the database is clean before a test suite starts
-  const conn = await mongoose.connect(
-    `${process.env.MONGODB_URI}/${config.Database}`
-  );
-
+  // Make sure the database is clean before the test suite starts
+  const conn = await mongoose.connect(process.env.MONGODB_URI);
   await conn?.connection?.db?.dropDatabase();
   await mongoose.disconnect();
 }
