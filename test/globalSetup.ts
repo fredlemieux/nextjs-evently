f8f85f365fbf06e953f71aa111fb94b948b63fe0
Frederique Lemieux <fredlemi@gmail.com>
2024-11-01 15:02:08 +0100

    Remove the config setup for mongo memory database, and simplify setup massively

--- test/globalSetup.ts
+++ test/globalSetup.ts
@@ -1,24 +1,13 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
-import * as mongoose from 'mongoose';
-import { config } from './utils/config';
-import { uuidv4 } from 'mongodb-memory-server-core/lib/util/utils';
 
 export default async function globalSetup() {
-  if (config.Memory) {
-    // Global MongoMemoryServer instance
-    const instance = await MongoMemoryServer.create();
-    const uri = instance.getUri();
-    (global as any).__MONGOINSTANCE = instance;
+  // Global MongoMemoryServer instance
+  const instance = await MongoMemoryServer.create();
+  const uri = instance.getUri();
+  (global as any).__MONGOINSTANCE = instance;
 
-    // Generate a unique database name for each test suite
-    const uniqueDbName = uuidv4();
-    process.env.MONGODB_URI = `${uri.slice(0, uri.lastIndexOf('/'))}/${uniqueDbName}`;
-  } else {
-    process.env.MONGODB_URI = `mongodb://${config.IP}:${config.Port}/${config.Database}`;
-  }
-
-  // Make sure the database is clean before the test suite starts
-  const conn = await mongoose.connect(process.env.MONGODB_URI);
-  await conn?.connection?.db?.dropDatabase();
-  await mongoose.disconnect();
+  // Generate a base uri string for each test suite
+  const mongoUri = `${uri.slice(0, uri.lastIndexOf('/'))}`;
+  // Create a base URL for all tests to utilise...
+  process.env.MONGODB_URI_BASE = mongoUri;
 }
