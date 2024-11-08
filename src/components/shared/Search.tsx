50bb603f015681c262713d431b4e492ea61d1d43
Frederique Lemieux <fredlemi@gmail.com>
2024-11-08 02:09:34 +0100

    Use lucide icons and replace old icon in search

--- src/components/shared/Search.tsx
+++ src/components/shared/Search.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
-import Image from 'next/image';
+import { SearchIcon } from 'lucide-react';
 import { formUrlQuery, removeKeysFromQuery } from '@/lib/utils';
 import { Input } from '../ui/input';
 
@@ -40,12 +40,7 @@ const Search = ({
 
   return (
     <div className='flex-center min-h-[54px] w-full overflow-hidden rounded-md bg-grey-50 px-4 py-2'>
-      <Image
-        src='/assets/icons/search.svg'
-        alt='search'
-        width={24}
-        height={24}
-      />
+      <SearchIcon className='h-6 w-6 stroke-gray-500' />
       <Input
         type='text'
         placeholder={placeholder}
