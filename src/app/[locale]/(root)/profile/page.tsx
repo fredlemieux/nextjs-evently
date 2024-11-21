fbbf70ef26f67fe1e34341164da8bbef3f1685a1
Frederique Lemieux <fredlemi@gmail.com>
2024-11-21 02:15:36 +0100

    Translate Profile

--- src/app/[locale]/(root)/profile/page.tsx
+++ src/app/[locale]/(root)/profile/page.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
 import Link from 'next/link';
 import { auth } from '@clerk/nextjs/server';
 import { getEventsByUser } from '@/lib/actions/event.actions';
 import Collection from '@/components/shared/Collection';
 import { Button } from '@/components/ui/button';
 import type { SearchParamProps } from '@/types/parameters.types';
+import { getTranslations } from 'next-intl/server';
 
 const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   const { sessionClaims } = await auth();
   const userId = sessionClaims?.userId as string;
+  const t = await getTranslations('Profile');
 
   const eventsPage = Number(searchParams?.eventsPage) || 1;
 
@@ -18,9 +19,9 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
     <>
       <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
         <div className='wrapper flex items-center justify-center sm:justify-between'>
-          <h3 className='h3-bold text-center sm:text-left'>Events Organized</h3>
+          <h3 className='h3-bold text-center sm:text-left'>{t('title')}</h3>
           <Button asChild size='lg' className='button hidden sm:flex'>
-            <Link href='/events/create'>Create New Event</Link>
+            <Link href='/events/create'>{t('create')}</Link>
           </Button>
         </div>
       </section>
