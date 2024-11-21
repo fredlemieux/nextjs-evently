f8a42562bdf5d038f60d34021b0b8ddce6dd3630
Frederique Lemieux <fredlemi@gmail.com>
2024-11-21 01:40:37 +0100

    Get translations on serverside

--- src/app/[locale]/(root)/page.tsx
+++ src/app/[locale]/(root)/page.tsx
@@ -19,6 +19,8 @@ export default async function Home({ searchParams }: SearchParamProps) {
     limit: 6,
   });
 
+  const t = await getTranslations('Landing');
+
   return (
     <>
       <section className='bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10'>
