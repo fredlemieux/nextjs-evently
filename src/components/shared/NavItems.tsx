74888aec5b24ff64b931183f61703bb9b6735b62
Frederique Lemieux <fredlemi@gmail.com>
2024-11-24 00:17:39 +0100

    remove console log

--- src/components/shared/NavItems.tsx
+++ src/components/shared/NavItems.tsx
@@ -4,11 +4,11 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { headerLinks } from '@/constants';
 import { useTranslations } from 'next-intl';
+import { SignOutButton } from '@clerk/nextjs';
 
 const NavItems = () => {
   const pathName = usePathname();
   const t = useTranslations('Header');
-  console.log('T_HELLO_:', t('hello'));
 
   return (
     <ul className='md:flex-between item-start flex w-full flex-col gap-5 md:flex-row'>
