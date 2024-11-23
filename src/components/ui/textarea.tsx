6d66b7e0d6e54ad943745958db702b34fd286f4f
Frederique Lemieux <fredlemi@gmail.com>
2024-11-24 00:19:06 +0100

    Text area tailwind class simplified

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -8,10 +8,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
     return (
       <textarea
-        className={cn(
-          'bg-background ring-offset-background flex min-h-[80px] w-full rounded-md border border-input px-3 py-2 text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-          className
-        )}
+        className={cn('text-area text-sm', className)}
         ref={ref}
         {...props}
       />
