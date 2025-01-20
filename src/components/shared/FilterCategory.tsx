c881f935c3569da69490ba6b134e7a9127c72ab7
Frederique Lemieux <fredlemi@gmail.com>
2025-01-20 10:16:37 +0100

    Add className to props for FilterCategory component

--- src/components/shared/FilterCategory.tsx
+++ src/components/shared/FilterCategory.tsx
@@ -15,7 +15,11 @@ import { formUrlQuery, removeKeysFromQuery } from '@/lib/utils';
 import type { ICategory } from '@/lib/database/models/category.model';
 import type { ToJSON } from '@/types/utility.types';
 
-const FilterCategory = () => {
+interface Props {
+  className: string;
+}
+
+const FilterCategory = ({ className }: Props) => {
   const [categories, setCategories] = useState<ToJSON<ICategory>[]>([]);
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -53,8 +57,8 @@ const FilterCategory = () => {
 
   return (
     <Select onValueChange={(value: string) => onSelectCategory(value)}>
-      <SelectTrigger className='input-wrapper'>
-        <SelectValue placeholder='Category' />
+      <SelectTrigger className={`input-wrapper ${className}`}>
+        <SelectValue placeholder='All' />
       </SelectTrigger>
       <SelectContent>
         <SelectItem value='All' className='select-item p-regular-14'>
